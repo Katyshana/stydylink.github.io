0b274944c8e2d3bb0df7a145178250c11f4668a5
ridis <99031897+Katyshana@users.noreply.github.com>
2023-11-12 13:51:06 +0200

    Add files via upload

---
+++ kursova/firebase/fb.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBx1CTo-Cjla5mVJ599lzN8gveBrcrl9Os",
+  authDomain: "black-kirill.firebaseapp.com",
+  projectId: "black-kirill",
+  storageBucket: "black-kirill.appspot.com",
+  messagingSenderId: "1004907917915",
+  appId: "1:1004907917915:web:d0b4c768c523c0a649457e"
+};
+
+  firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+var provider = new firebase.auth.GoogleAuthProvider();
